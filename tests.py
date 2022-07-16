513b89197c4cf2cffa95a0b8ffdcb4edda16a67e
Andrei Gul <gul_a@bk.ru>
2022-07-16 16:59:23 +0300

    Добавлены 2 теста: set_book_rating() и get_book_rating()

--- tests.py
+++ tests.py
@@ -20,5 +20,29 @@ class TestBooksCollector:
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
         assert len(collector.get_books_rating()) == 2
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    #Тест на проверку установки рейтинга книги set_book_rating()
+    def test_set_book_rating_add_rating_for_consist_book_is_set(self):
+
+        #Создаем объект и добавляем книгу
+        collector = BooksCollector()
+        collector.add_new_book('Братья Карамазовы')
+
+        #Устанавливаем рейтинг книге
+        collector.set_book_rating('Братья Карамазовы', 7)
+
+        #Проверка на корректность
+        assert collector.books_rating['Братья Карамазовы'] == 7
+
+    #Тест на корректность получения рейтинга по названию книги get_book_rating()
+    def test_get_book_rating_name_book_is_correct_rating(self):
+        #Создаем объект, добавляем книгу, устанавливаем рейтинг
+        collector = BooksCollector()
+        book_name = 'Война и мир'
+        rate = 10
+
+        collector.add_new_book(book_name)
+        collector.set_book_rating(book_name, rate)
+
+        #Вызываем метод get_books_rating() и проверяем соответствие
+        assert collector.get_book_rating(book_name) == rate
+
