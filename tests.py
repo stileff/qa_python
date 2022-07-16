84d2edc8fab3bd5db5ab83ad9cb6f31929ba699f
Andrei Gul <gul_a@bk.ru>
2022-07-16 18:04:44 +0300

    Добавлено 3 теста для: get_books_with_specific_rating(), add_book_in_favorites(), get_list_of_favorites_books()

--- tests.py
+++ tests.py
@@ -46,3 +46,48 @@ class TestBooksCollector:
         #Вызываем метод get_books_rating() и проверяем соответствие
         assert collector.get_book_rating(book_name) == rate
 
+    #Проверяем, что get_books_with_specific_rating() правильно получает список
+    def test_get_books_with_specific_rating_filter_rating_list_length_is_correct(self):
+        #Создаем список из книг
+        dict_with_books = {
+            'Золотой ключик или приключения Буратино': 4,
+            'Властелин Колец': 10,
+            'Чума': 10,
+            'Алые паруса': 7
+        }
+        specific_rating = 10
+
+        collector = BooksCollector()
+
+        #Добавляем список книг в объект BooksCollector
+        for key, value in dict_with_books.items():
+            collector.add_new_book(key)
+            collector.set_book_rating(key, value)
+
+        #Получаем список книг соответствующих specific_rating
+        lst_filtred_books_by_specific_rating = collector.get_books_with_specific_rating(specific_rating)
+
+        assert len(lst_filtred_books_by_specific_rating) == 2
+
+    #Проверяем добавление книги в favorities методом add_book_in_favorites
+    def test_add_book_in_favorites_name_is_added_to_favorite(self):
+        book_name = 'Пришвин. Кладовая солнца'
+
+        collector = BooksCollector()
+        collector.add_new_book(book_name)
+
+        assert collector.books_rating[book_name]
+
+    #Проверяем, что метод get_list_of_favorites_books() правильно возвращает список книг
+    def test_get_list_of_favorites_books_list_length_is_correct(self):
+        # Создаем список из книг
+        collector = BooksCollector()
+
+        lst_of_books = ['Зеленая Миля', 'Дон Кихот', 'Гроза', 'Ромео и джульетта', 'Снеговик']
+        for book in lst_of_books:
+            collector.add_new_book(book)
+
+        collector.add_book_in_favorites(lst_of_books[1])
+        collector.add_book_in_favorites(lst_of_books[3])
+
+        assert len(collector.get_list_of_favorites_books()) == 2
